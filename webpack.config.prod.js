1a39ced216060a026fc4fb6eebce16430227ba5d
chen chao <chao.wings@gmail.com>
2017-08-25 03:06:10 +0800

    extract and minify css

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from "webpack-md5-hash";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
 
 export default {
   debug: true,
@@ -18,6 +19,9 @@ export default {
     filename: '[name].[chunkhash].js'
   },
   plugins: [
+      // generate an external css file with hash in the filename
+      new ExtractTextPlugin('[name].[contenthash].css'),
+
       // hash file name using md5 so that their name only change when file changed
       new WebpackMd5Hash(),
 
@@ -54,7 +58,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
